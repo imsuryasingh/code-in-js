6adf7ee1c98ef362e6aa656e3c93d6db486f0b78
Surya Singh <123587710+imsuryasingh@users.noreply.github.com>
2024-05-10 22:35:36 +0000

    conter

--- .devcontainer/01_basic/test.js
+++ .devcontainer/01_basic/test.js
@@ -1,9 +1,14 @@
 let value = 2 
 let negValue = -value
-console.log(negValue)
+// console.log(negValue)
 
-console.log(1+"2");
-console.log("1"+2);
-console.log(1+2);
-console.log("1"+2+2);
-console.log(1+2+"3");
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log(1+2);
+// console.log("1"+2+2);
+// console.log(1+2+"3");
+
+// Game Counter
+let gameCounter = 100
+gameCounter ++;
+console.log(gameCounter);
